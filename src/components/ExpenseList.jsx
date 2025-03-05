16101f7b9bf097bdb8484aa7f1cf4060757a5d59
dainamnguyen3@gmail.com <dainamnguyen3@gmail.com>
2025-03-05 22:16:36 +0700

    update

--- src/components/ExpenseList.jsx
+++ src/components/ExpenseList.jsx
@@ -18,29 +18,34 @@ function ExpenseList(props) {
     <div>
       <h3 className="font-semibold mt-3 ">History</h3>
       <div class="border-b border-gray-900 mb-5"></div>
-      <ul>
-        {transactions.map((transaction) => (
-          <li
-            key={transaction.id}
-            className="flex items-center justify-between m-1 pl-4 bg-white shadow border-black "
-          >
-            <span className="flex-1">{transaction.category}</span>
-            <span
-            // className={`font-medium ${
-            //   transaction.type === "income"
-            //     ? "text-green-600"
-            //     : "text-red-600"
-            // }`}
+      <div>
+        <ul>
+          {transactions.map((transaction) => (
+            <li
+              key={transaction.id}
+              className=" group flex items-center justify-between m-1 pl-4 bg-white shadow border-black relative h-auto"
             >
-              {transaction.type === "income"
-                ? `+${transaction.amount}`
-                : transaction.amount}
-            </span>
+              <button className="bg-red-500 text-white text-xs w-4 h-4  rounded-none absolute left-[-17px] hidden group-hover:block">
+                X
+              </button>
+              {/* nội dung giao dịch */}
+              <span className=" ml-5">{transaction.category}</span>
+              <span className="mr-5">
+                {transaction.type === "income"
+                  ? `+${transaction.amount}`
+                  : transaction.amount}
+              </span>
 
-            <div class=" border-l border-gray-300 h-10 mt-1 w-4"></div>
-          </li>
-        ))}
-      </ul>
+              <div
+                className={`  absolute h-full mt-1 w-1 right-0 top-[-3.8px]
+            bg ${
+              transaction.type === "income" ? "bg-green-500 " : "bg-red-500"
+            }`}
+              ></div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
